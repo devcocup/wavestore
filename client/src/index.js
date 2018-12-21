747ac3249380b8c2e00ea7a106c9dd327df0e3a7
typescript59 <typescript59@gmail.com>
2018-12-22 00:28:06 +0530

    CLIENT Header anf footer

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './Resources/css/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+
+const App = (
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+);
+
+ReactDOM.render( App, document.getElementById('root'));
