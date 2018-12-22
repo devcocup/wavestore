7e75b0d2c1afb7fd3ac7e7d4bacbcdf0659e672a
typescript59 <typescript59@gmail.com>
2018-12-23 02:02:49 +0530

    CLIENT Creating the Register

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/index';
 import Layout from './HOC/layout';
 import RegisterLogin from './components/Register_login/index';
+import Register from './components/Register_login/register';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register" exact component={Register}/>
         <Route path="/register_login" exact component={RegisterLogin}/>
         <Route path="/" exact component={Home}/>
       </Switch>
