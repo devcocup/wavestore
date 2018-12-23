7ae2340e879acfcc7a83d4216d6adf80949124aa
typescript59 <typescript59@gmail.com>
2018-12-23 16:14:42 +0530

    CLIENT product request by Arrival & Sell

---
+++ client/src/actions/products_actions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { PRODUCT_SERVER } from './../components/utils/misc';
+import { 
+    GET_PRODUCTS_BY_ARRIVAL,
+    GET_PRODUCTS_BY_SELL
+} from './types';
+
+export function getProductsBySell() {
+    // sortBy=sold&order=desc&limit=4
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
+        .then(response => response.data);
+
+        return {
+            type: GET_PRODUCTS_BY_SELL,
+            payload: request
+        }
+}
+
+export function getProductsByArrival() {
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
+        .then(response => response.data);
+
+        return {
+            type: GET_PRODUCTS_BY_ARRIVAL,
+            payload: request
+        }
+}
