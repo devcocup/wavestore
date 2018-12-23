f9e5c8d76412317a056f8905c212b41d1dd70a0f
typescript59 <typescript59@gmail.com>
2018-12-23 15:39:12 +0530

    CLIENT Home Promotions Banner

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import HomeSlider from './home_slider';
+import HomePromotions from './home_promotion';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <HomeSlider />
+                <HomePromotions />
             </div>
         )
     }
