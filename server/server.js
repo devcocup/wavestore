1ca7cfe55345a2c1295ac24db5ae884a793beda4
typescript59 <typescript59@gmail.com>
2019-01-07 02:29:04 +0530

    try 1 push

--- server/server.js
+++ server/server.js
@@ -475,7 +475,7 @@ app.post('/api/site/site_data', auth, admin, (req,res) => {
 if(process.env.NODE_ENV === 'production'){
     
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+        res.sendfile(path.resolve(__dirname, '../client', 'build', 'index.html'))
     })
 }
 
